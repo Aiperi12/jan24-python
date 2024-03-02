8c619bf727b2439a868837264963ce289743f997
Aiperi12 <160559953+Aiperi12@users.noreply.github.com>
2024-03-02 14:09:48 -0600

    Update hw2-1.py

--- homework2/hw2-1.py
+++ homework2/hw2-1.py
@@ -5,10 +5,9 @@ import re       #re - regular expression module,
 
 print("Create a password with following requiurements: \n At least 8 characters long \n At least one uppercase letter \n At least one lowercase letter \n At least one digit \n At least one special character")
 print ("           ")
+
+while True:
 password = input ("Create a password: ")
-
-
-
 if len (password) < 8:
     print ("The password must be at least 8 characters")
 elif not re.search("[a-z]", password):
@@ -21,4 +20,6 @@ elif not re.search("[~!@#$%^&*]", password):
     print("The password must contain at least 1 special character")
 else:
     print ("Password accepted")  
+break  
+    
 
